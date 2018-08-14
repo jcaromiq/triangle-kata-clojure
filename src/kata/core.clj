16777d7e39cfb21e147a37b7200033998da3c8f5
Joaquin Caro <joaquin.caro@apiumhub.com>
2018-08-14 14:34:05 +0200

    Refactor

--- src/kata/core.clj
+++ src/kata/core.clj
@@ -1,20 +1,31 @@
 (ns kata.core)
 
-(defn triangle?
+(defn- sizes-are-valid? [sizes]
+  (let [max (first (reverse (sort sizes)))
+        minsum (apply + (rest (reverse (sort sizes))))]
+        (> minsum max)))
+
+(defn- only-three-sides?
   [sides]
   (= (count (filter pos-int? sides)) 3))
 
-(defn sizes-equals
+(defn- well-formed-triangle?
+  [sides]
+    (and (only-three-sides? sides) (sizes-are-valid? sides)))
+
+
+(defn- sizes-equals
   [sizes]
   (apply max (vals (frequencies sizes))))
 
 (defmulti kind (fn [sides] (sizes-equals sides)))
-(defmethod kind 3 [sides] "equilateral")
-(defmethod kind 2 [sides] "isosceles")
-(defmethod kind :default [sides] "scalene")
+
+(defmethod kind 3 [_] "equilateral")
+(defmethod kind 2 [_] "isosceles")
+(defmethod kind :default [_] "scalene")
 
 (defn kind-of-triangle
   [sides]
-  (if (triangle? sides)
+  (if (well-formed-triangle? sides)
     (kind sides)
     (throw (IllegalArgumentException.))))
--- test/kata/core_should.clj
+++ test/kata/core_should.clj
@@ -1,32 +1,36 @@
 (ns kata.core_should
   (:require [midje.sweet :refer :all]
-            [kata.core :refer :all])
-  )
+            [kata.core :refer :all]))
 
 (facts
-  "About Type of triangle given the length of the sides"
-  (fact
-    "Should throw exception given more than three sides"
-    (kind-of-triangle [1 2 3 4]) => (throws IllegalArgumentException))
+ "About Type of triangle given the length of the sides"
+ (fact
+  "Should throw exception given more than three sides"
+  (kind-of-triangle [1 2 3 4]) => (throws IllegalArgumentException))
 
-  (fact
-    "Should throw exception given sides as not numerical"
-    (kind-of-triangle ["a" 2 3]) => (throws IllegalArgumentException))
+ (fact
+  "Should throw exception given sides as not numerical"
+  (kind-of-triangle ["a" 2 3]) => (throws IllegalArgumentException))
 
-  (fact
-    "Should throw exception given sides with size not positive"
-    (kind-of-triangle [0 2 3]) => (throws IllegalArgumentException))
+ (fact
+  "Should throw exception given sides with size not positive"
+  (kind-of-triangle [0 2 3]) => (throws IllegalArgumentException))
 
-  (fact
-    "Should return equilateral if sides are equals"
-    (kind-of-triangle [1 1 1]) => "equilateral")
 
-  (fact
-    "Should return isosceles if only two sides are equals"
-    (kind-of-triangle [1 1 2]) => "isosceles")
+ (fact
+  "Should throw exception if the sum of sides is incorrect"
+  (kind-of-triangle [1 1 2]) => (throws IllegalArgumentException))
 
-  (fact
-    "Should return scalene if sides aren't equals"
-    (kind-of-triangle [1 2 3]) => "scalene")
 
-  )
+ (fact
+  "Should return equilateral if sides are equals"
+  (kind-of-triangle [2 2 2]) => "equilateral")
+
+ (fact
+  "Should return equilateral if sides are equals"
+  (kind-of-triangle [1 2 2]) => "isosceles")
+
+
+ (fact
+  "Should return scalene if sides aren't equals"
+  (kind-of-triangle [3 2 4]) => "scalene"))
